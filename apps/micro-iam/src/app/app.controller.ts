6945eaa3bf6c0e6be0892ee5ef97ac4c65a6949d
Carlo Casorzo <carlo@deepblu.dev>
2022-11-09 20:22:36 +0100

    refactor: add ack to IAM status rmq controller

--- apps/micro-iam/src/app/app.controller.ts
+++ apps/micro-iam/src/app/app.controller.ts
@@ -1,10 +1,22 @@
 import { Controller } from '@nestjs/common'
-import { MessagePattern } from '@nestjs/microservices'
+import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices'
+
+import { RmqService } from '@lean/shared/infra/comms'
 
 @Controller()
 export class AppController {
+  constructor(private readonly rmqService: RmqService) {}
   @MessagePattern('status')
-  status() {
+  status(
+    @Payload() payload: Record<string, unknown>,
+    @Ctx() context: RmqContext
+  ) {
+    console.log('RECEIVED in IAM: ', {
+      message: context.getPattern(),
+      payload,
+    })
+    this.rmqService.ack(context)
+
     return {
       message: '[IAM] All systems operational',
     }
--- libs/shared/infra/comms/src/rabbitmq/rabbitmq.config.ts
+++ libs/shared/infra/comms/src/rabbitmq/rabbitmq.config.ts
@@ -1,7 +1,7 @@
 import { RabbitMQ } from '@lean/shared/infra/comms'
 
 export const rmqConfig = () => ({
-  rmqUri: process.env.RABBITMQ_URI || 'amqp://rmq-broker:5672',
+  rmqUri: process.env.RABBITMQ_URI || 'amqp://user:pass@rmq-broker:5672',
   rmqQueue: (queue: string) =>
     process.env[RabbitMQ.QUEUE(queue)] || RabbitMQ.QUEUE(queue),
 })
