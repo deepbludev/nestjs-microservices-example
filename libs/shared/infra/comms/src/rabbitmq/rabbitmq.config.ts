8b237ad784036debed3ea10d50f88e402b257151
Carlo Casorzo <56896917+deepblue369@users.noreply.github.com>
2022-11-10 15:21:43 +0100

    chore: use default guest/guest credentials in rmq

--- libs/shared/infra/comms/src/rabbitmq/rabbitmq.config.ts
+++ libs/shared/infra/comms/src/rabbitmq/rabbitmq.config.ts
@@ -1,7 +1,7 @@
 import { RabbitMQ } from './rabbitmq.constants'
 
 export const rmqConfig = () => ({
-  rmqUri: process.env.RABBITMQ_URI || 'amqp://user:pass@rmq-broker:5672',
+  rmqUri: process.env.RABBITMQ_URI || 'amqp://guest:guest@rmq-broker:5672',
   rmqQueue: (queue: string) =>
     process.env[RabbitMQ.QUEUE(queue)] || RabbitMQ.QUEUE(queue),
 })
