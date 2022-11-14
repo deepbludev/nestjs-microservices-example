ca411c0c1ba1046396c58408d92ae2286dcdf907
Carlo Casorzo <carlo@deepblu.dev>
2022-11-14 05:57:36 +0100

    refactor: remove listening to port in IAM microservice

--- apps/micro/iam/src/main.ts
+++ apps/micro/iam/src/main.ts
@@ -1,16 +1,14 @@
 import { Logger } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
+// import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { Exchange } from '@obeya/shared/infra/comms'
 
 import { IamMicroserviceModule } from './app/iam.microservice.module'
 
 async function bootstrap() {
   const iam = await NestFactory.create(IamMicroserviceModule)
-  const config = iam.get(ConfigService)
-  const port = config.get('port')
-
-  await iam.listen(port)
-  Logger.log(`🚀 IAM Microservice is running on ${await iam.getUrl()}`)
+  await iam.init()
+  Logger.log(`🚀 IAM Microservice is running on amqp queue: ${Exchange.IAM}`)
 }
 
 bootstrap()
