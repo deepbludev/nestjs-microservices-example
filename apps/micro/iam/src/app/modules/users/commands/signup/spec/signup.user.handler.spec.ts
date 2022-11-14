76c14805ff1c0831d4bd8eb5731e320c3c49975f
Carlo Casorzo <56896917+deepblue369@users.noreply.github.com>
2022-11-14 11:22:55 +0100

    test: ensure SignupUserHandler return ok when email and password are valid

--- apps/micro/iam/src/app/modules/users/commands/signup/spec/signup.user.handler.spec.ts
+++ apps/micro/iam/src/app/modules/users/commands/signup/spec/signup.user.handler.spec.ts
@@ -1,23 +1,28 @@
+import { CommandResponse, Result } from '@deepblu/ddd'
+import { SignupUser, signupUserDTOStub } from '@obeya/contexts/iam/domain'
+import { CommandBus } from '@obeya/shared/infra/comms'
+
 import { SignupUserHandler } from '../signup.user.handler'
 
 describe(SignupUserHandler, () => {
-  // const ctrl = new SignupUserAmqpRpcController(new CommandBus([]))
-  // describe('#signup', () => {
-  //   describe('when email and password are valid', () => {
-  //     it('returns status 201 Created', async () => {
-  //       const dto = {
-  //         id: 'cce2fded-90cd-4ec9-8806-842834e73e6c',
-  //         email: 'valid@email.com',
-  //         password: 'valid_password',
-  //       }
-  //       const response = await ctrl.run(dto)
-  //       expect(response).toEqual({
-  //         data: { id: dto.id },
-  //         status: 201,
-  //         message: `User ${dto.email} created`,
-  //       })
-  //     })
-  //   })
-  // })
-  it.todo('TODO')
+  const handler = new SignupUserHandler()
+  const commandbus = new CommandBus([handler])
+
+  describe('#handle', () => {
+    describe('when email and password are valid', () => {
+      let response: Awaited<CommandResponse>
+
+      beforeAll(async () => {
+        const dto = signupUserDTOStub()
+        response = await commandbus.dispatch(SignupUser.with(dto))
+      })
+
+      it.todo('delegates validation to User factory')
+      it.todo('delegates persistence to repo')
+
+      it('returns Result.ok()', async () => {
+        expect(response).toEqual(Result.ok())
+      })
+    })
+  })
 })
