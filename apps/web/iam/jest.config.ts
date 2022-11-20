e48d254c66b0e046e26368472f2becf06a3b2dd1
Carlo Casorzo <carlo@deepblu.dev>
2022-11-20 13:46:23 +0100

    chore: change web-obeya app name to web-iam

--- apps/web/iam/jest.config.ts
+++ apps/web/iam/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'web-obeya',
+  displayName: 'web-iam',
   preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/apps/web/obeya',
+  coverageDirectory: '../../../coverage/apps/web/iam',
 }
