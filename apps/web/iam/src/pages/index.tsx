97f047760c09e94487a1f95a0b5d3c46706cbd5e
Carlo Casorzo <56896917+deepblue369@users.noreply.github.com>
2022-11-21 10:22:10 +0100

    refactor: change h1 to Mantine Title

--- apps/web/iam/src/pages/index.tsx
+++ apps/web/iam/src/pages/index.tsx
@@ -1,13 +1,14 @@
+import { Title } from '@mantine/core'
 import { ExampleButton } from '@obeya/shared/ui/design-system'
 
 export function Index() {
   return (
     <>
       <div id="welcome">
-        <h1>
+        <Title>
           <span> Hello there, </span>
           Welcome obeya 👋
-        </h1>
+        </Title>
         <p className="text-red-600 font-bold">Hello, world!</p>
       </div>
       <ExampleButton color="green" size="xl">
