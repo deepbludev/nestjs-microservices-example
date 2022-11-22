60f7d270826a8c1b785b0f9ad0461a36f98cc2b3
Carlo Casorzo <56896917+deepblue369@users.noreply.github.com>
2022-11-22 11:41:59 +0100

    refactor: use axios for fetching

--- apps/web/iam/src/pages/api/status/index.ts
+++ apps/web/iam/src/pages/api/status/index.ts
@@ -1,9 +1,15 @@
+import axios from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<{ message: string }>
 ) {
   console.log({ req: req.body })
-  return res.status(200).json({ message: 'Next.js API operational' })
+
+  const { data } = await axios<{
+    message: string
+  }>('http://localhost:3000/status')
+
+  res.status(200).json(data)
 }
--- apps/web/iam/src/pages/index.tsx
+++ apps/web/iam/src/pages/index.tsx
@@ -1,24 +1,56 @@
 import { Button, Title } from '@mantine/core'
 import { ExampleButton } from '@obeya/shared/ui/design-system'
 import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
+export type CommandEndpoint = {
+  verb: 'post'
+  path: string
+}
+
+export type QueryEndpoint = {
+  verb: 'get'
+  path: string
+}
+
+export const API = {
+  status: { verb: 'get', path: 'api/status' },
+  iam: {
+    users: {
+      signup: { verb: 'post', path: 'api/iam/users/signup' },
+      login: { verb: 'post', path: 'api/iam/users/login' },
+    },
+  },
+} as const
+
+export function query<T>(endpoint: QueryEndpoint) {
+  return () => axios.get<T>(endpoint.path).then(res => res.data)
+}
+
+export function command<T>(endpoint: QueryEndpoint) {
+  return () => axios.post<T>(endpoint.path).then(res => res.data)
+}
+
+type Response = { message: string }
 
 export function Index() {
-  const { isLoading, data } = useQuery({
+  const { isLoading, isError, error, data } = useQuery({
     queryKey: ['status'],
-    queryFn: () =>
-      fetch('http://localhost:3000/status').then(res => res.json()),
+    queryFn: query<Response>(API.status),
   })
 
-  return isLoading ? (
-    'loading...'
-  ) : (
+  if (isLoading) return 'loading...'
+  if (isError) return `error: ${error}`
+
+  const { message } = data
+  return (
     <>
       <div id="welcome">
         <Title>
           <span> Hello, obeya! 👋</span>
         </Title>
         <p className="text-action-80 bg-bnw-90 font-bold">Hello, world!</p>
-        <p>{JSON.stringify(data)}</p>
+        <p>{message}</p>
       </div>
 
       <ExampleButton color="action">Click me</ExampleButton>
