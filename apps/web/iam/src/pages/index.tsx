1138e2cd20d042e70aa082b628169e576bf09448
Carlo Casorzo <carlo@deepblu.dev>
2022-11-23 06:40:01 +0100

    feat: implement draft of getQuery/useQuery and sendCommand/useCommand

--- apps/web/iam/src/pages/index.tsx
+++ apps/web/iam/src/pages/index.tsx
@@ -11,36 +11,15 @@ export type QueryEndpoint = {
   query: string
 }
 
-export const API = {
-  status: { query: 'api/status' },
-  iam: {
-    users: {
-      signup: { command: 'api/iam/users/signup' },
-      login: { command: 'api/iam/users/login' },
-    },
-  },
-} as const
-
-export function query<T>(endpoint: QueryEndpoint) {
-  return () => axios.get<T>(endpoint.query).then(res => res.data)
-}
-
-export function command<T>(endpoint: CommandEndpoint) {
-  return () => axios.post<T>(endpoint.command).then(res => res.data)
-}
-
-type Response = { message: string }
-
 export function Index() {
-  const { isLoading, isError, error, data } = useQuery({
-    queryKey: ['status'],
-    queryFn: query<Response>(API.status),
+  const { isLoading, isError, error, data } = useQuery(['status'], {
+    queryFn: () =>
+      axios.get<{ message: string }>('api/status').then(res => res.data),
   })
 
   if (isLoading) return 'loading...'
   if (isError) return `error: ${error}`
 
-  const { message } = data
   return (
     <>
       <div id="welcome">
@@ -48,7 +27,7 @@ export function Index() {
           <span> Hello, obeya! 👋</span>
         </Title>
         <p className="text-action-80 bg-bnw-90 font-bold">Hello, world!</p>
-        <p>{message}</p>
+        <p>{data?.message}</p>
       </div>
 
       <ExampleButton color="action">Click me</ExampleButton>
