2f998a37c41fae0dd9a163e707822891a37756bf
Carlo Casorzo <56896917+deepblue369@users.noreply.github.com>
2022-12-13 13:41:27 +0100

    chore: add enableShutdownHooks() to nest apps

--- apps/micro/iam/src/main.ts
+++ apps/micro/iam/src/main.ts
@@ -6,6 +6,7 @@ import { IamMicroserviceModule } from './app/iam.microservice.module'
 
 async function bootstrap() {
   const iam = await NestFactory.create(IamMicroserviceModule)
+  iam.enableShutdownHooks()
   await iam.init()
   Logger.log(`🚀 IAM Microservice is running on amqp queue: ${Context.IAM}`)
 }
